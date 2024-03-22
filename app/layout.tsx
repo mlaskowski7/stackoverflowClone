55832a6585fd5aa72ba153af87ec0253fe03e43c
Mateusz Laskowski <mateuszlaskowski@Mateuszs-Air.lan>
2024-03-22 20:45:00 +0100

    setting up fonts, metadata and clerk appearance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,29 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevOverflow",
+  description:
+    "Explore mine Next.js project, a simplified clone of Stack Overflow designed for fast, interactive learning and knowledge sharing. Built with Next.js and Tailwind CSS, it features a user-friendly Q&A platform for developers to ask questions, share solutions, and enhance their coding skills. Dive into a community-driven environment that mirrors the best of Stack Overflow's collaborative spirit.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,9 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
