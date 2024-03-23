4fbfaf1143ef06a2012032ad21a62636cd725658
mlaskowski7 <mateuszlaskowski@Mateuszs-MacBook-Air.local>
2024-03-23 21:20:55 +0100

    setting up navbar ui

---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    // eslint-disable-next-line tailwindcss/no-custom-classname
+    <nav className="flex-between background-light900_dark200 x-50 fixed w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/assets/images/site-logo.svg"
+          alt="DevOverflow logo"
+          width={23}
+          height={23}
+        />
+
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="text-primary-500">Overflow</span>
+        </p>
+      </Link>
+      {/* GlobalSearch */}
+      <div className="flex-between gap-5">
+        Theme
+        <SignedIn>
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+            afterSignOutUrl="/"
+          />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
