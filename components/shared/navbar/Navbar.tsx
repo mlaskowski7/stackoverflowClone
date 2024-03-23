4f5fb4abcaf88054912b8438f4ed5603ed06c4aa
mlaskowski7 <mateuszlaskowski@Mateuszs-MacBook-Air.local>
2024-03-23 23:50:36 +0100

    finished navbar and also mobile view navbar - responsive design

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,10 +2,12 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
-    // eslint-disable-next-line tailwindcss/no-custom-classname
     <nav className="flex-between background-light900_dark200 x-50 fixed w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
@@ -19,9 +21,9 @@ const Navbar = () => {
           Dev<span className="text-primary-500">Overflow</span>
         </p>
       </Link>
-      {/* GlobalSearch */}
+      <GlobalSearch />
       <div className="flex-between gap-5">
-        Theme
+        <Theme />
         <SignedIn>
           <UserButton
             appearance={{
@@ -35,6 +37,7 @@ const Navbar = () => {
             afterSignOutUrl="/"
           />
         </SignedIn>
+        <MobileNav />
       </div>
     </nav>
   );
