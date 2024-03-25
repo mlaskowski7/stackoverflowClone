6d70e04579e4d99c463989533c0fa009c280b801
mlaskowski7 <mateuszlaskowski@Mateuszs-Air.lan>
2024-03-25 23:45:30 +0100

    left sidebar

---
+++ components/shared/leftSidebar/LeftSidebar.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
+const LeftSidebar = () => {
+  const pathname = usePathname();
+  return (
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+      <div className="flex h-full flex-col gap-4">
+        {sidebarLinks.map((link, index) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <div key={index}>
+              <Link
+                href={link.route}
+                className={`${isActive ? "primary-gradient rounded-lg text-light-900" : "text-dark300_light900"} flex items-center justify-start gap-4 bg-transparent p-4`}
+              >
+                <Image
+                  src={link.imgURL}
+                  alt={link.label}
+                  width={20}
+                  height={20}
+                  className={`${isActive ? "" : "invert-colors"}`}
+                />
+                <p
+                  className={`${isActive ? "base-bold" : "base-medium"} max-lg:hidden`}
+                >
+                  {link.label}
+                </p>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+
+      <SignedOut>
+        <div className="flex flex-col gap-3">
+          <Link href="/sign-in">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/account.svg"
+                alt="signin"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">
+                Sign in
+              </span>
+            </Button>
+          </Link>
+
+          <Link href="/sign-up">
+            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/sign-up.svg"
+                alt="signup"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="max-lg:hidden">Sign up</span>
+            </Button>
+          </Link>
+        </div>
+      </SignedOut>
+    </section>
+  );
+};
+
+export default LeftSidebar;
