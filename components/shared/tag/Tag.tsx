de4419a1d1b57c254b25f471c1b734bcbc31dd11
mlaskowski7 <mateuszlaskowski@Mateuszs-MacBook-Air.local>
2024-03-27 18:14:19 +0100

    right sidebar, tag and localsearch components

---
+++ components/shared/tag/Tag.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+
+import { Badge } from "@/components/ui/badge";
+
+interface TagProps {
+  id: number;
+  name: string;
+  questionsNumber?: number;
+  showCount?: boolean;
+}
+
+const Tag = ({ id, name, questionsNumber, showCount }: TagProps) => {
+  return (
+    <Link href={`/tags/${id}`} className="flex justify-between gap-2 ">
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+        {name}
+      </Badge>
+
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{questionsNumber}</p>
+      )}
+    </Link>
+  );
+};
+
+export default Tag;
