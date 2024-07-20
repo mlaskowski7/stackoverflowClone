449e430e70ee9824ce84414d5b4e8b5ddf339107
mlaskowski7 <mtlaskowski7@gmail.com>
2024-07-20 09:32:51 +0200

    collections page

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,63 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import NoResults from "@/components/shared/NoResults/NoResults";
+import Filter from "@/components/shared/filter/Filter";
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import { QuestionFilters } from "@/constants/filters";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { use } from "react";
+
+export default async function Home() {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const result = await getSavedQuestions({
+    clerkId: userId,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Questions</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions..."
+          otherClasses="flex-1"
+        />{" "}
+        <Filter
+          filters={QuestionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
+            <QuestionCard
+              key={question.id}
+              id={question.id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={new Date(question.createdAt)}
+            />
+          ))
+        ) : (
+          <NoResults
+            title="There are no saved questions to show at the moment"
+            link="/ask-question"
+            button="Click to ask a Question"
+          />
+        )}
+      </div>
+    </>
+  );
+}
--- components/shared/votes/Votes.tsx
+++ components/shared/votes/Votes.tsx
@@ -5,6 +5,7 @@ import {
   downvoteQuestion,
   upvoteQuestion,
 } from "@/lib/actions/question.action";
+import { toggleSaveQuestion } from "@/lib/actions/user.action";
 import { formatBigNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
@@ -33,7 +34,13 @@ const Votes = ({
   hasSaved,
 }: Props) => {
   const pathname = usePathname();
-  const handleSave = () => {};
+  const handleSave = async () => {
+    await toggleSaveQuestion({
+      userId: JSON.parse(userId),
+      questionId: JSON.parse(itemId),
+      path: pathname,
+    });
+  };
 
   const handleVote = async (action: string) => {
     if (!userId) {
